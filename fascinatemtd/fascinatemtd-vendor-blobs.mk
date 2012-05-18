550f520639e7d146babe63c4e24cbf583069ba32
Stevespear426 <stephen.k.spear@gmail.com>
2012-05-18 23:17:04 +0200

    Add Glitch lights lib for fascinate.
    Also added some other Glitchy goodies and fixes that I've been using on
    my Glitched up builds.
    
    Change-Id: Ic1f3afbdc3db3794f38bd6e02d8c2d7e7789a299

--- fascinatemtd/fascinatemtd-vendor-blobs.mk
+++ fascinatemtd/fascinatemtd-vendor-blobs.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/fascinatemtd/proprietary/libril.so:system/lib/libril.so \
     vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/fascinatemtd/proprietary/libsec-ril40.so:system/lib/libsec-ril40.so \
+    vendor/samsung/fascinatemtd/proprietary/lights.aries.so:system/lib/hw/lights.aries.so \
     vendor/samsung/fascinatemtd/proprietary/rild:system/bin/rild \
     vendor/samsung/fascinatemtd/proprietary/pppd_runner:system/bin/pppd_runner \
     vendor/samsung/fascinatemtd/proprietary/gpsd:system/vendor/bin/gpsd \
@@ -32,4 +33,17 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/fascinatemtd/proprietary/geomagneticd:system/vendor/bin/geomagneticd \
     vendor/samsung/fascinatemtd/proprietary/orientationd:system/vendor/bin/orientationd \
     vendor/samsung/fascinatemtd/proprietary/ip-up:system/etc/ppp/ip-up \
-    vendor/samsung/fascinatemtd/proprietary/ip-down:system/etc/ppp/ip-down
+    vendor/samsung/fascinatemtd/proprietary/ip-down:system/etc/ppp/ip-down \
+    vendor/samsung/fascinatemtd/proprietary/cleaningmodules:script/cleaningmodules \
+    vendor/samsung/fascinatemtd/proprietary/nstools.sh:script/nstools.sh \
+    vendor/samsung/fascinatemtd/proprietary/logcat_module:system/etc/init.d/logcat_module \
+    vendor/samsung/fascinatemtd/proprietary/S99screenstate_scaling:system/etc/init.d/S99screenstate_scaling \
+    vendor/samsung/fascinatemtd/proprietary/screenstate_scaling:system/etc/glitch-config/screenstate_scaling \
+    vendor/samsung/fascinatemtd/proprietary/sleep_governor:system/etc/glitch-config/sleep_governor \
+    vendor/samsung/fascinatemtd/proprietary/logcat-off:system/bin/logcat-off \
+    vendor/samsung/fascinatemtd/proprietary/logcat-on:system/bin/logcat-on \
+    vendor/samsung/fascinatemtd/proprietary/sysinit:system/bin/sysinit \
+    vendor/samsung/fascinatemtd/proprietary/lib.sh:system/bin/lib.sh \
+    vendor/samsung/fascinatemtd/proprietary/datafix:system/bin/datafix \
+    vendor/samsung/fascinatemtd/proprietary/callboost:system/bin/callboost
+
---
+++ fascinatemtd/proprietary/logcat-on
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+su -c "cp /system/lib/modules/logger.ko /data/local/logger.ko"
+echo
+echo
+echo "Reboot to take effect"
