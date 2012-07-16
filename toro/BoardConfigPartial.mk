c241eea80719a00a96fde521978a41d48bf44a3e
Adam Fisch <adammfisch@gmail.com>
2012-07-16 08:05:03 -0600

    updated props for 4.1.1 (forked from xoomdev & rootzboat)

--- toro/BoardConfigPartial.mk
+++ toro/BoardConfigPartial.mk
@@ -11,7 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-LOCAL_STEM := toro/device-partial.mk
-
-$(call inherit-product-if-exists, vendor/samsung/$(LOCAL_STEM))
---
+++ toro/device-partial.mk
@@ -0,0 +1,65 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Samsung blob(s) necessary for Toro hardware
+PRODUCT_PACKAGES := \
+    AndroidInSpace.240p \
+    AndroidInSpace.480p \
+    bcm4330 \
+    BIP \
+    Disco.240p \
+    Disco.480p \
+    ducati-m3 \
+    gps.omap4 \
+    gralloc.omap4 \
+    IMSFramework \
+    RTN \
+    SDM \
+    SyncMLSvc \
+    fRom \
+    libdrmwvmplugin \
+    libEGL_POWERVR_SGX540_120 \
+    libGLESv1_CM_POWERVR_SGX540_120 \
+    libGLESv2_POWERVR_SGX540_120 \
+    libglslcompiler \
+    libIMGegl \
+    libinvensense_mpl \
+    libpn544_fw \
+    libpvr2d \
+    libpvrANDROID_WSEGL \
+    libPVRScopeServices \
+    libsecril-client \
+    libsrv_init \
+    libsrv_um \
+    libsyncml_core \
+    libsyncml_port \
+    lib_gsd4t \
+    libims \
+    libims_jni \
+    libsec-ril_lte \
+    libusc \
+    libwvdrm_L1 \
+    libwvm \
+    libWVphoneAPI \
+    libWVStreamControlAPI_L1 \
+    LMprec_508 \
+    PFFprec_600 \
+    pvrsrvctl \
+    sirfgps \
+    smc_pa_wvdrm \
+    Sunset240p \
+    Sunset480p \
+    SuperSU \
+    VZWAPNLib \
+    VZWAPNService
