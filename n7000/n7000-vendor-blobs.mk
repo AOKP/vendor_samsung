87d3e26cd557400732ed38775086b8951c079c6d
XpLoDWilD <xplodgui@gmail.com>
2012-07-17 17:52:47 +0200

    Derp
    Conflicts:
    	n7000/n7000-vendor-blobs.mk
    
    Change-Id: I8a967549abc545bfc5d66183d74d0e6fc2fe6f6d

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -16,6 +16,73 @@
 PRODUCT_COPY_FILES := \
     vendor/samsung/n7000/proprietary/system/lib/libril.so:obj/lib/libril.so \
     vendor/samsung/n7000/proprietary/system/lib/libsecril-client.so:obj/lib/libsecril-client.so
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libTVOut.so:obj/lib/libTVOut.so \
+    $(LOCAL_PATH)/proprietary/audio/lib/libmediayamahaservice.so:obj/lib/libmediayamahaservice.so \
+    $(LOCAL_PATH)/proprietary/ril/lib/libril.so:obj/lib/libril.so \
+    $(LOCAL_PATH)/proprietary/ril/lib/libsecril-client.so:obj/lib/libsecril-client.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libhdmi.so:obj/lib/libhdmi.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libhdmiclient.so:obj/lib/libhdmiclient.so
+
+# EGL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/graphics/lib/libMali.so:system/lib/libMali.so \
+    $(LOCAL_PATH)/proprietary/graphics/lib/libUMP.so:system/lib/libUMP.so \
+    $(LOCAL_PATH)/proprietary/graphics/lib/libion.so:system/lib/libion.so \
+    $(LOCAL_PATH)/proprietary/graphics/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
+    $(LOCAL_PATH)/proprietary/graphics/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    $(LOCAL_PATH)/proprietary/graphics/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
+    $(LOCAL_PATH)/proprietary/graphics/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
+    $(LOCAL_PATH)/proprietary/graphics/lib/hw/gralloc.exynos4.so:system/lib/hw/gralloc.exynos4.so
+
+# HWCOMPOSER - needs tvout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/hw/hwcomposer.exynos4.so:system/lib/hw/hwcomposer.exynos4.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/bin/bintvoutservice:system/bin/bintvoutservice \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libcec.so:system/lib/libcec.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libddc.so:system/lib/libddc.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libedid.so:system/lib/libedid.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libfimc.so:system/lib/libfimc.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libfimc.so:obj/lib/libfimc.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libhdmi.so:system/lib/libhdmi.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libhdmiclient.so:system/lib/libhdmiclient.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libTVOut.so:system/lib/libTVOut.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libtvout_jni.so:system/lib/libtvout_jni.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libtvoutinterface.so:system/lib/libtvoutinterface.so \
+    $(LOCAL_PATH)/proprietary/hwcomposer/lib/libtvoutservice.so:system/lib/libtvoutservice.so
+	
+# CAMERA
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/camera/lib/hw/camera.exynos4.so:system/lib/hw/vendor-camera.exynos4.so \
+    $(LOCAL_PATH)/proprietary/camera/lib/libcaps.so:system/lib/libcaps.so \
+    $(LOCAL_PATH)/proprietary/camera/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
+    $(LOCAL_PATH)/proprietary/camera/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv
+	
+# SENSORS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/sensors/lib/hw/sensors.exynos4.so:system/lib/hw/sensors.exynos4.so \
+    $(LOCAL_PATH)/proprietary/sensors/lib/libakm.so:system/lib/libakm.so
+	
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/gps/lib/hw/gps.exynos4.so:system/lib/hw/gps.exynos4.so \
+    $(LOCAL_PATH)/proprietary/gps/bin/gpsd:system/bin/gpsd \
+    $(LOCAL_PATH)/proprietary/gps/bin/gps.cer:system/bin/gps.cer \
+    $(LOCAL_PATH)/proprietary/gps/etc/gps.xml:system/etc/gps.xml
+	
+# WIFI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/wifi/bin/bcm4330B1.hcd:system/bin/bcm4330B1.hcd \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/bcm4330_apsta.bin:system/etc/wifi/bcm4330_apsta.bin \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/bcm4330_mfg.bin:system/etc/wifi/bcm4330_mfg.bin \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/bcm4330_p2p.bin:system/etc/wifi/bcm4330_p2p.bin \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/bcm4330_sta.bin:system/etc/wifi/bcm4330_sta.bin \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/nvram_mfg.txt_murata:system/etc/wifi/nvram_mfg.txt_murata \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/nvram_mfg.txt_semcove:system/etc/wifi/nvram_mfg.txt_semcove \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/nvram_net.txt_murata:system/etc/wifi/nvram_net.txt_murata \
+    $(LOCAL_PATH)/proprietary/wifi/etc/wifi/nvram_net.txt_semcove:system/etc/wifi/nvram_net.txt_semcove \
 
 PRODUCT_COPY_FILES += \
     vendor/samsung/n7000/proprietary/system/bin/rild:system/bin/rild \
