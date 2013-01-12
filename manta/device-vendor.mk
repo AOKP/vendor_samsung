7b01f1d84ce8c9f787c6182bd301bac3f8d156ee
xoomdev <xoomdevnet@gmail.com>
2013-01-11 19:54:19 -0600

    Add manta blobs (JOP40D)
    Change-Id: Iee7f60202ba05cdcb788e80598c84ef973dffc1a

---
+++ manta/device-vendor.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES := \
+    bcm2079x_firmware \
+    bcm2079x_pre_firmware \
+    bcm43241 \
+    es305_fw \
+    fimc_is_fw \
+    fimc_is_fw2 \
+    libbccArm.sha1 \
+    libbccArm \
+    libbcinfoArm \
+    libclcoreArm \
+    libclcore_neonArm \
+    libdrmdecrypt \
+    libdrmwvmplugin \
+    libGLES_mali \
+    libRSDriverArm \
+    libwvdrm_L1 \
+    libwvm \
+    libWVStreamControlAPI_L1 \
+    maxtouch \
+    mfc_fw \
+    setfile \
+    setfile_4e5 \
+    setfile_6a3 \
+    00060308060501020000000000000000 \
+    020a0000000000000000000000000000 \
+    07060000000000000000000000000000
