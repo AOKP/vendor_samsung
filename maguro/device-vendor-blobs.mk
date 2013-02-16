231bcdb4ef583f852724ff92738d5a43b4e9bd54
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-02-16 14:53:06 +0100

    maguro: Update to 4.2.2 props
    Change-Id: I2d1c7f7aa03912dcc1418633f02d311bf2616ca9
    
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- maguro/device-vendor-blobs.mk
+++ maguro/device-vendor-blobs.mk
@@ -42,5 +42,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/maguro/proprietary/libsec-ril.so:system/vendor/lib/libsec-ril.so \
     vendor/samsung/maguro/proprietary/libsrv_init.so:system/vendor/lib/libsrv_init.so \
     vendor/samsung/maguro/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so \
-    vendor/samsung/maguro/proprietary/libusc.so:system/vendor/lib/libusc.so
+    vendor/samsung/maguro/proprietary/libusc.so:system/vendor/lib/libusc.so \
+    vendor/samsung/maguro/proprietary/libbt-vendor.so:system/vendor/lib/libbt-vendor.so
 
