ee03ca995f40741bb3ae466fbbb721dd8f6579e2
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-02-16 14:53:06 +0100

    tuna: Refactor proprietary blobs
    Also update to 4.2.2 - maguro/toro/toroplus
    
    Change-Id: I2d1c7f7aa03912dcc1418633f02d311bf2616ca9
    
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

---
+++ tuna/device-vendor-blobs.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Samsung blob(s) necessary for maguro hardware
+PRODUCT_PACKAGES += \
+    bcm4330 \
+    ducati-m3 \
+    fRom \
+    gps.omap4 \
+    gralloc.omap4 \
+    libdrmdecrypt \
+    libdrmwvmplugin \
+    libEGL_POWERVR_SGX540_120 \
+    libGLESv1_CM_POWERVR_SGX540_120 \
+    libGLESv2_POWERVR_SGX540_120 \
+    libglslcompiler \
+    libIMGegl \
+    libinvensense_mpl \
+    libpn544_fw \
+    libpvr2d \
+    libpvrANDROID_WSEGL \
+    libPVRScopeServices \
+    libsecril-client \
+    libsrv_init \
+    libsrv_um \
+    libusc \
+    libwvdrm_L1 \
+    libwvm \
+    libWVStreamControlAPI_L1 \
+    pvrsrvctl \
+    sirfgps
+
